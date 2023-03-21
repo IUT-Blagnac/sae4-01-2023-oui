55a0a3e27a961fd12d17cd9a7f42d4cbc851051f
Furiza31 <hugo.wendjaneh@gmail.com>
2023-03-21 19:13:39 +0100

    Add create.sql into jar file

--- belote/src/main/java/fr/blagnac/com/Belote.java
+++ belote/src/main/java/fr/blagnac/com/Belote.java
@@ -2,6 +2,7 @@ package fr.blagnac.com;
   
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.Connection;  
 import java.sql.DriverManager;  
 import java.sql.SQLException;
@@ -45,15 +46,17 @@ public class Belote {
 
 				String dos = System.getProperty("user.dir");
 				String beloteFile = dos + "/jBelote";
-				String createFile = "create.sql";
+				String createFile = "/create.sql";
 				System.out.println("Dossier de stockage:" + beloteFile);
 				if (!new File(beloteFile).isDirectory()) {
 					new File(beloteFile).mkdir();
 				}
 				connection = DriverManager.getConnection("jdbc:hsqldb:file:" + beloteFile + "/belote", "sa", "");
 				statement = connection.createStatement();
-	
-				importSQL(connection, new File(createFile));
+
+				// Get the SQL script
+				InputStream createFileInputStream = Belote.class.getResourceAsStream(createFile);
+				importSQL(connection, createFileInputStream);
 				
 			}catch(SQLException e){
 				JOptionPane.showMessageDialog(null, "Impossible de se connecter à la base de donn�e. Vérifier qu'une autre instance du logiciel n'est pas déjà ouverte.");
@@ -76,7 +79,7 @@ public class Belote {
 	}
 
 	
-	public static void importSQL(Connection conn, File in) throws SQLException, FileNotFoundException
+	public static void importSQL(Connection conn, InputStream in) throws SQLException, FileNotFoundException
 	{
 	        Scanner s = new Scanner(in);
 	        s.useDelimiter("(;(\r)?\n)|(--\n)");
